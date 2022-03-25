ee86dd22c2d6dcde4f1c5c5e65d28d6b9aece175
hellomac87 <hellomac87@gmail.com>
2022-03-25 09:13:07 +0900

    feat: card ui

---
+++ src/components/card/StarRate/index.tsx
@@ -0,0 +1,29 @@
+import clsx from 'clsx';
+
+import styles from './index.module.css';
+
+type Props = {
+    rate?: 1 | 2 | 3 | 4 | 5;
+};
+
+const starCount = 5;
+
+function StarRate({ rate }: Props) {
+    if (!rate) {
+        return null;
+    }
+
+    return (
+        <div className={styles.container}>
+            {Array(starCount)
+                .fill(0)
+                .map((_, i) => i + 1)
+                .map((idx) => {
+                    const active = idx <= rate;
+                    return <div className={clsx(styles.star, { [styles.active]: active })} key={idx} />;
+                })}
+        </div>
+    );
+}
+
+export default StarRate;
---
+++ src/components/card/index.tsx
@@ -0,0 +1,37 @@
+import Comment from './Comment';
+import StarRate from './StarRate';
+
+import styles from './index.module.css';
+import clsx from 'clsx';
+
+export type CardProps = {
+    image: string;
+    label: string;
+    title: string;
+    rate?: 1 | 2 | 3 | 4 | 5;
+    direction?: CardDirectionType;
+    comment?: string;
+};
+
+export type CardDirectionType = 'vertical' | 'horizental';
+
+function Card({ image, label, title, rate, direction = 'vertical', comment }: CardProps) {
+    return (
+        <div className={clsx(styles.container, styles[direction])}>
+            <div className={styles.thumbnail} style={{ backgroundImage: `url(${image})` }} />
+            <div className={styles.body}>
+                <div className={styles.info}>
+                    <label className={styles.label}>{label}</label>
+                    <div className={styles.title}>{title}</div>
+                </div>
+
+                <div className={clsx(styles.rating, { [styles.visible]: Boolean(rate) })}>
+                    <StarRate rate={rate} />
+                    <Comment comment={comment} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
